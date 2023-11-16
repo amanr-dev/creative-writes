47329d54bdec235e53012801d4bc151af71925dd
Aman <dv451197@gmail.com>
2023-11-16 22:24:30 +0530

    fixed profile image

--- components/Nav.js
+++ components/Nav.js
@@ -64,7 +64,7 @@ export default function Nav() {
             fontSize: "1.1rem",
             alignItems: "center",
             justifyContent: "center",
-            gap: "1.5rem",
+            gap: "0.5rem",
           }}
         >
           <Link
